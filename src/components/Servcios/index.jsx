368da387a969fdd7464b03afa3ea894742e20447
mago_d_Mercurio <davila.feria@gmail.com>
2023-07-06 00:37:57 -0500

    add Contacto Component Servicios update

--- src/components/Servcios/index.jsx
+++ src/components/Servcios/index.jsx
@@ -6,19 +6,31 @@ const Servicios = () => {
   return (
     <>
     
-    <span className="custom-block-subtitle " >Servicios</span>
+    <span className="custom-block-big-title text-customPrimary " > Mis Servicios</span>
     <ul className="mt-6 mb-6 text-white">
       <li className="mb-4 flex">
         <CheckArrowIcon />
-        <span>Creacion de Landing-Pages</span>
+        <span>Terapia Psicológica: A través de sesiones individuales, te brindo un espacio seguro y confidencial donde exploraremos tus desafíos emocionales y desarrollaremos estrategias efectivas para superarlos.</span>
       </li>
       <li className="mb-4 flex">
         <CheckArrowIcon />
-        <span>Experiencias interactivas de calidad</span>
+        <span>Tratamientos contra Depresión y Ansiedad: Utilizando enfoques terapéuticos Cognitivo-Conductual, te brindo apoyo y herramientas para hacer frente a la depresión y la ansiedad, fomentando tu bienestar mental.</span>
       </li>
       <li className="mb-4 flex">
         <CheckArrowIcon />
-        <span>Optimizado de la experiencia web para dispositivos móviles</span>
+        <span>Problemas Emocionales: Trabajaremos juntos para abordar una amplia gama de problemas emocionales, como el manejo del estrés, la ira, el miedo, la tristeza y otros desafíos que afectan tu equilibrio emocional.</span>
+      </li>
+      <li className="mb-4 flex">
+        <CheckArrowIcon />
+        <span>Entendimiento Personal: A través de un enfoque de introspección, te ayudaré a comprenderte mejor. Exploraremos tu pasado y cómo está afectando tu presente, brindándote una visión más clara de ti mismo/a y permitiéndote tomar mejores decisiones en tu vida.</span>
+      </li>
+      <li className="mb-4 flex">
+        <CheckArrowIcon />
+        <span>Acompañamiento en la visión de tu pasado: Trabajaremos juntos para explorar y comprender cómo tu pasado ha influido en tu vida actual. Mediante un enfoque compasivo, te apoyaré en el proceso de sanación y te ayudaré a liberarte de patrones y limitaciones que puedan estar afectando tu bienestar.</span>
+      </li>
+      <li className="mb-4 flex">
+        <CheckArrowIcon />
+        <span>Mi objetivo es proporcionarte un apoyo integral y personalizado para que puedas alcanzar una vida más plena y satisfactoria. Cada uno de estos servicios se adapta a tus necesidades individuales, brindándote las herramientas y la orientación necesarias para tu crecimiento personal y emocional.</span>
       </li>
     </ul>
     </>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import Layout from "../../components/Layout"
 import Hero from "../../components/Hero"
 import SobreMi from "../../components/SobreMi"
 import Servicios from "../../components/Servcios"
+import Contacto from "../../components/Contacto"
 
 
 
@@ -11,7 +12,7 @@ const Home = () => {
       <Hero />
       <SobreMi />
       <Servicios />
-    
+      <Contacto />
     </Layout>
   )
 }
