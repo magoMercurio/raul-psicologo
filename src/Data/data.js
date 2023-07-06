7232d084e98189592dc104c048132508cc3ecd9d
mago_d_Mercurio <davila.feria@gmail.com>
2023-07-06 00:05:35 -0500

    add data.js and refactor SobreMi Component

---
+++ src/Data/data.js
@@ -0,0 +1,35 @@
+
+import aboutImg1 from '../assets/imgs/logo.png'
+import aboutImg2 from '../assets/imgs/logo.png'
+import aboutImg3 from '../assets/imgs/logo.png'
+import aboutImg4 from '../assets/imgs/logo.png'
+import aboutImg5 from '../assets/imgs/logo.png'
+
+
+export const aboutData = [
+  {
+    title: 1,
+    description: 'Durante más de 5 años, he tenido el privilegio de acompañar a personas en su camino hacia la superación de problemas y la búsqueda de una vida más tranquila y plena.',
+    image: aboutImg1,
+  },
+  {
+    title: 2,
+    description: 'Combino los mejores elementos de la terapia psicológica con el poderoso método de la Bio-neuroemoción. Esta combinación única me permite abordar los desafíos desde diferentes perspectivas, facilitando un proceso de transformación profunda y duradera.',
+    image: aboutImg2,
+  },
+  {
+    title: 3,
+    description: ' Mi misión es ayudar a las personas a encontrar la paz mental y emocional que tanto anhelan, apoyando en su proceso de cambio y crecimiento. Explorando nuevas perspectivas y descubriendo fortalezas ocultas, desarrollando estrategias efectivas para superar los obstáculos que impiden alcanzar el bienestar emocional.',
+    image: aboutImg3,
+  },
+  {
+    title: 4,
+    description: 'Creo en la importancia de crear un espacio seguro y acogedor para mis clientes, donde puedan sentirse escuchados y comprendidos en cada paso del camino.',
+    image: aboutImg4,
+  },
+  {
+    title: 5,
+    description: 'Mi enfoque se basa en una colaboración activa y empática, adaptando mis técnicas y herramientas a las necesidades individuales de cada persona.',
+    image: aboutImg5,
+  }
+]
--- src/components/SobreMi/index.jsx
+++ src/components/SobreMi/index.jsx
@@ -1,13 +1,5 @@
 import { motion } from "framer-motion";
-
-import { CheckArrowIcon } from "../../assets/icons/CheckArrowIcon";
-
-import section1 from '../../assets/imgs/logo.png'
-import section2 from '../../assets/imgs/logo.png'
-import section3 from '../../assets/imgs/logo.png'
-import section4 from '../../assets/imgs/logo.png'
-
-
+import { aboutData } from '../../Data/data.js'
 
 const SobreMi = () => {
   return (
@@ -21,51 +13,38 @@ const SobreMi = () => {
       viewport={{ once: true }}
       transition={{ duration: 0.5, delay: 0.2 }}
     >
-      <div className="flex flex-col items-center justify-center  md:pl-4 xl:pr-16 xl:pl-16 mt-20">
+      <div className="flex flex-col items-center justify-center  md:pl-4 xl:pr-16 xl:pl-16 ">
         <div className="w-full  mb-10 ">
           <div className="flex flex-col items-center justify-center ">
-            <h2 className="mt-6 mb-8  custom-block-big-title text-customLightBg3 text-center ml-20 mr-20">
+            <h2 className="mt-6 mb-20  custom-block-big-title text-customLightBg3 text-center ml-20 mr-20">
               Terapeuta licenciado en Psicología con formación en Bioneuroemoción.
             </h2>
-            <div className="flex flex-col lg:flex-row lg:justify-around items-center gap-8 ">
-              <div className="flex flex-col lg:flex-row items-center gap-8 p-6">
-                <p className="mb-10 text-blue-500 leading-loose">
-                Durante más de 5 años, he tenido el privilegio de acompañar a personas en su camino hacia la superación de problemas y la búsqueda de una vida más tranquila y plena.
-                </p>
-                <img src={section1} alt="" className="w-[40%] " />
+
+            <div className="grid justify-items-center grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-8 lg:grid-cols-3 lg:gap-5 xl:gap-10 px-6 xl:px-0 items-center mt-10">
+            {aboutData.map((about, index) => (
+              <div
+                className="w-11/12 h-fit sm:w-4/5 md:w-auto md:h-[350px] lg:w-auto custom-border-gray-darker rounded-xl bg-customDarkBg3 flex flex-col justify-start px-6 py-4 my-5"
+                key={`${about.title} - ${index}`}
+              >
+                <div className="flex justify-start">
+                  <img src={about.image} alt="" width="50px" />
+                </div>
+                <div className="flex mt-4 mb-2 xl:mt-8 xl:mb-4">
+                  <div className="flex flex-col ml-4">
+                    <div className="custom-content-text-gray">
+                      {about.description}
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="flex flex-col lg:flex-row items-center gap-8 p-6">
-                <p className="mb-10 text-blue-500 leading-loose">
-                  Combino los mejores elementos de la terapia psicológica con el poderoso método de la Bio-neuroemoción. Esta combinación única me permite abordar los desafíos desde diferentes perspectivas, facilitando un proceso de transformación profunda y duradera.
-                </p>
-                <img src={section1} alt="" className="w-[40%] " />
-              </div>
+              ))}
             </div>
           </div>
         </div>
-        <div className=" flex justify-center ">
-          <div className="mb-8 lg:mb-0 w-full sm:w-1/2 px-2 lg:px-0">
-            <div className="mb-4 py-3 pl-3 pr-2 rounded">
-            <p className=" text-blue-500 leading-loose">
-              Mi misión es ayudar a las personas a encontrar la paz mental y emocional que tanto anhelan. 
-              </p>
-              </div>
-              <div className="py-3 pl-3 pr-2 rounded ">
-                <p className=" text-blue-500 leading-loose">
-                Creo en la importancia de crear un espacio seguro y acogedor para mis clientes, donde puedan sentirse escuchados y comprendidos en cada paso del camino.
-                </p>
-              </div>
-              <div className="py-3 pl-3 pr-2 rounded ">
-                <p className=" text-blue-500 leading-loose">
-                Mi enfoque se basa en una colaboración activa y empática, adaptando mis técnicas y herramientas a las necesidades individuales de cada persona.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </motion.div>
+      </div>
+    </motion.div>
     </section>
-    )
+  )
 }
 
 export default SobreMi
