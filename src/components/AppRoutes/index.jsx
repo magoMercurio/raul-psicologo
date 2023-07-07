15c5f0391f798e1ab1bef66f83b4f39b0df9f7d2
mago_d_Mercurio <davila.feria@gmail.com>
2023-07-07 15:41:54 -0500

    move approutes

--- src/components/AppRoutes/index.jsx
+++ src/components/AppRoutes/index.jsx
@@ -1,6 +1,6 @@
 import { useRoutes } from 'react-router-dom'
 
-import Home from '../../Pages/Home'
+import Home from '../../pages/Home'
 import Blog from '../../Pages/Blog'
 import NotFound from '../../Pages/NotFound'
 
--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,9 +1,22 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, useRoutes } from 'react-router-dom'
 import  './App.css'
-import AppRoutes from '../../components/AppRoutes'
 import NavBar from '../../components/NavBar'
 import Footer from '../../components/Footer'
 
+
+import Home from '../../pages/Home'
+import Blog from '../../Pages/Blog'
+import NotFound from '../../Pages/NotFound'
+
+const AppRoutes = () => {
+  const routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/blog', element: <Blog />},
+    {path: '*', element: <NotFound /> }
+
+  ])
+  return routes
+}
 
 function App() {
 
