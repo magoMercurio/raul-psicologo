5585fccc71637cd90e9f8be260d3f7421588b15f
mago_d_Mercurio <davila.feria@gmail.com>
2023-07-07 13:02:15 -0500

    functional Form only console.log

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -7,12 +7,14 @@ const Form = () => {
   console.log(errors);
   
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder="Nombre" {...register("Nombre", {required: true, maxLength: 80})} />
-      <input type="email" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
-      <input type="tel" placeholder="Telefono" {...register("Telefono", {maxLength: 12})} />
+    <form onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col items-center gap-4"
+    >
+      <input type="text" placeholder="Nombre" className="input input-bordered input-info border-customPrimary w-full max-w-xs" {...register("Nombre", {required: true, maxLength: 80})} />
+      <input type="email" placeholder="Email" className="input input-bordered input-info border-customPrimary w-full max-w-xs" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
+      <input type="tel" placeholder="Telefono" className="input input-bordered input-info  border-customPrimary w-full max-w-xs" {...register("Telefono", {maxLength: 12})} />
 
-      <button type="submit"> Enviar </button>
+      <button type="submit" className='btn custom-button-colored w-36  mb-5'> Enviar </button>
     </form>
   );
 }
