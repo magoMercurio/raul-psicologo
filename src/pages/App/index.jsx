df5d1248aa321f43e53e364fdd26f2bdfcd83164
mago_d_Mercurio <davila.feria@gmail.com>
2023-07-07 15:46:30 -0500

    Update index.jsx

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -6,13 +6,13 @@ import Footer from '../../components/Footer'
 
 import Home from '../../pages/Home'
 /* import Blog from '../../Pages/Blog' */
-import NotFound from '../../Pages/NotFound'
+/* import NotFound from '../../Pages/NotFound' */
 
 const AppRoutes = () => {
   const routes = useRoutes([
     { path: '/', element: <Home /> },
     /* { path: '/blog', element: <Blog />}, */
-    {path: '*', element: <NotFound /> }
+    /* {path: '*', element: <NotFound /> } */
 
   ])
   return routes
